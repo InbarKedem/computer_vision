95909411d1e0d76ede391b37504df98dbe550b1d
InbarKedem <inkedem@gmail.com>
2020-07-28 15:48:00 +0300

    add firsts labeled images

--- code/my_project.py
+++ code/my_project.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import cv2
 
 print("hello inbar")
@@ -6,13 +7,15 @@ print("hello inbar")
 ## machine vision pipe
 
 # data loader
-path = r'C:\Users\inked\PycharmProjects\test\dataset\image_1.jpeg'
+path = r'C:\Users\inked\PycharmProjects\computer_vision\data_set\un-labeled_images\test1.NEF'
 img = cv2.imread(path)
-cv2.imwrite(r'C:\Users\inked\PycharmProjects\test\saved_images\new_img.png', img)
-#
+cv2.imwrite(r'/data_set/larvea/labeled_images\new_test1.png', img)
+#loop that for all of the pictures
 
 
 # split data to training, validation and test 60%, 20%, 20%
+
+
 # scipy split dataset
 
 # data preperation, define augmentation
